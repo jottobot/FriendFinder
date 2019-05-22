9bf079df1e2355f35ea705b0e2bb8461d1ed457e
Jordynn Otto <j_taylor92@yahoo.com>
2019-05-21 21:31:15 -0700

    compare function

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 // DATA
 // Below data will hold all of the friends.
 
-var friendsArray = [
+var friends = [
   {
     "name":"Sarah",
     "photo":"https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwjUh8Sy9q3iAhUBOn0KHcoIAT4QjRx6BAgBEAU&url=https%3A%2F%2Fpngtree.com%2Ffreepng%2Fcartoon-girl_1273131.html&psig=AOvVaw15YoZ3oIoGOAnhBDqc_t0c&ust=1558572829035299",
@@ -21,4 +21,4 @@ var friendsArray = [
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
-module.exports = friendsArray;
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,36 +1,49 @@
 // Requiring data from friends.js
-var friends = require("../data/friends");
+var friends = require('../data/friends');
 
 module.exports = function(app) {
   // API GET Requests
   // Below code handles when users visit survey page and returns friends as json
-  app.get("/api/friends", function(req, res) {
+  app.get('/api/friends', function(req, res) {
     res.json(friends);
   });
 
-  // API POST Requests
-  // When user POSTS (submits) data to server 
-  app.post("/api/friends", function(req, res) {
-     // This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+  // API POST Requests, when user POSTS (submits) data to server
+  app.post('/api/friends', function(req, res) {
+    // This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
     var newUser = req.body;
-    var newUserScore = 0;
-    var difference;
-    for (let i = 0; i < newUser['scores'].length; i++) {
-      newUserScore += newUser['scores'][i];
-  }
 
-     for (var i = 0; i < friends.length; i++) {
-       var currentUser = friend[i];
-       var currentUserScore = 0;
-     }
+    // var bestMatch = {
+    //   name: '',
+    //   photo: '',
+    //   friendDifference: Infinity
+    // };
 
-  //    Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
-  //  * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
-  //    * Example:
-  //      * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
-  //      * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
-  //      * Total Difference: **2 + 1 + 2 =** **_5_**
-  //  * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on.
+    var diff = 0;
+    for (var i = 0; i < friends.length; i++) {
+      // scores of a friend in the database
+      var friendScores = friends[i].scores;
+      // scores of the new friend that just submitted the survey
+      var newFriendScores = newUser.scores;
+      for (var j = 0; j < friendScores.length; j++) {
+        if (friendScores[j] > newFriendScores[j]) {
+          diff += friendScores[j] - newFriendScores[j];
+        } else {
+          diff += newFriendScores[j] - friendScores[j];
+        }
+      }
+      // if (diff <= bestMatch.friendDifference) {
+      //   // Reset the bestMatch to be the new friend.
+      //   bestMatch.name = newUser.name;
+      //   bestMatch.photo = newUser.photo;
+      //   bestMatch.friendDifference = diff;
+      // }
+    }
+    // Add the newUser object to the friends data table (array)
+    friends.push(newUser);
+    res.json();
 
+    // Return the closest match
+    
   });
 };
