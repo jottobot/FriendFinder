5c355a1afd4828e896186f922ee644220b07ec8d
Jordynn Otto <j_taylor92@yahoo.com>
2019-05-22 19:53:02 -0700

    final

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,25 +1,19 @@
 // Requiring data from friends.js
 var friends = require('../data/friends');
 
-module.exports = function(app) {
+module.exports = function (app) {
   // API GET Requests
   // Below code handles when users visit survey page and returns friends as json
-  app.get('/api/friends', function(req, res) {
+  app.get('/api/friends', function (req, res) {
     res.json(friends);
   });
 
   // API POST Requests, when user POSTS (submits) data to server
-  app.post('/api/friends', function(req, res) {
-    // This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+  app.post('/api/friends', function (req, res) {
+
     var newUser = req.body;
-
-    // var bestMatch = {
-    //   name: '',
-    //   photo: '',
-    //   friendDifference: Infinity
-    // };
-
     var diff = 0;
+    var scoreToBeat = 50;
     for (var i = 0; i < friends.length; i++) {
       // scores of a friend in the database
       var friendScores = friends[i].scores;
@@ -31,19 +25,15 @@ module.exports = function(app) {
         } else {
           diff += newFriendScores[j] - friendScores[j];
         }
+        if (diff < scoreToBeat) {
+          scoreToBeat = diff;
+          match = friends[i];
+        }
       }
-      // if (diff <= bestMatch.friendDifference) {
-      //   // Reset the bestMatch to be the new friend.
-      //   bestMatch.name = newUser.name;
-      //   bestMatch.photo = newUser.photo;
-      //   bestMatch.friendDifference = diff;
-      // }
     }
-    // Add the newUser object to the friends data table (array)
-    friends.push(newUser);
-    res.json();
 
-    // Return the closest match
-    
+    friends.push(newUser);
+    res.json(match);
+
   });
 };
